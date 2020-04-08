3cec427a151ffa443a669309a0a46f46a1027f39
Corey Scheideman <corscheid@gmail.com>
2020-04-07 21:55:04 -0400

    create simple database adapter for testing

--- src/tspg.ts
+++ src/tspg.ts
@@ -1,26 +1,23 @@
 import { Client } from 'ts-postgres';
  
 async function main() {
-    const client = new Client();
+    const client = new Client({'database':'baltimore911'});
     await client.connect();
  
     try {
         // Querying the client returns a query result promise
         // which is also an asynchronous result iterator.
         const resultIterator = client.query(
-            // `SELECT 'Hello ' || $1 || '!' AS message`,
-            // ['world']
-            `SELECT * FROM test`
+            `SELECT username, email FROM admins`
         );
  
         for await (const row of resultIterator) {
-            // 'Hello world!'
-            //console.log(row.get('message'));
-            console.log(row.get('testcolumn'));
+            console.log(`Username:`, row.get('username'));
+            console.log(`Email:`, row.get('email'));
         }
     } finally {
         await client.end();
     }
 }
- 
+
 main()
